2e697e08efa474acb7dfd60028bd5c0eec4e8bd8
JsonZhangAA <1942143988@qq.com>
2018-11-01 15:49:41 +0800

    testG++UseCuda

---
+++ testG++UseCuda/test.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"This CPU,then use GPU function:"<<endl;
+    //VectAdd();
+    return 0;
+}
---
+++ testG++UseCuda/test.cu
@@ -0,0 +1,8 @@
+#include <cutil.h>  
+//extern "C" void VectAdd(int *a, int *b, int *c, int length);  
+void VectAdd(int *a, int *b, int *c, int length)  
+{     
+    for(int i=0;i<5;i++){
+        printf("this is CUDA! Success!!!");
+    }
+}  
